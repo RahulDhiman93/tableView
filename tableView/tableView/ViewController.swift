0334a28e9195943bffb02b88c650c9f713336468
RahulDhiman93 <rsdm34@gmail.com>
2017-10-20 17:24:22 +0530

    initials

---
+++ tableView/tableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Rahul Dhiman on 20/10/17.
+//  Copyright © 2017 Rahul Dhiman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
